46a7de0fe67dc2e1751cbe4cddc8a152dd9e3e90
mskedelj <s43643@edu.campus02.at>
2025-05-16 15:21:31 +0200

    chore: logging zu ODataClient hinzugefügt. Products.json für ODataProductsService angepasst

--- ODataClient/Program.cs
+++ ODataClient/Program.cs
@@ -12,19 +12,32 @@ namespace ODataClient
 
 			// Beispielabfragen:
 			// 1. Alle Produkte mit ID < 5
+			Console.WriteLine("Produkte mit ID < 5:");
 			var cheapProducts = context.Products
 				.AddQueryOption("$filter", "Id lt 5")
 				.ToList();
+			Console.WriteLine("Anzahl Produkte: " + cheapProducts.Count);
+			Console.WriteLine("Produkte: " + string.Join(", ", cheapProducts.Select(p => p.Name)));
+			Console.WriteLine();
 
-			// 2. Nach ID gruppiert
+			// 2. Nach Name sortiert
+			Console.WriteLine("Produkte nach Name sortiert");
 			var byCategory = context.Products
-				.AddQueryOption("$orderby", "Id")
+				.AddQueryOption("$orderby", "Name")
 				.ToList();
+			Console.WriteLine("Anzahl Produkte: " + byCategory.Count());
+			Console.WriteLine("Produkte: " + string.Join(", ", byCategory.Select(p => p.Name)));
+			Console.WriteLine();
 
-			// 3. Nur Name und ID
+			// 3. Name und ID der Produkte
+			Console.WriteLine("Nur der Name der Produkte");
 			var projected = context.Products
-				.AddQueryOption("$select", "Id,Name")
+				.AddQueryOption("$select", "Id, Name")
 				.ToList();
+			Console.WriteLine("Anzahl Produkte: " + projected.Count());
+			Console.WriteLine("Produkte: " + string.Join(", ", projected.Select(p => p.Id + "-" + p.Name)));
+
+			Console.ReadKey();
 		}
 	}
 }
